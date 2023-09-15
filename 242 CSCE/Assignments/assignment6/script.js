f457da4b5ef7d3811304c52e1ff8acdeec856b5b
Colin Mathis Elmore <colin_elmore@Colins-MacBook-Pro-3.local>
2023-09-15 12:43:21 -0400

    ass6

---
+++ 242 CSCE/Assignments/assignment6/script.js
@@ -0,0 +1,57 @@
+const showPic = () => {
+    document.getElementById("pic").classList.remove("hide");
+}
+
+const hidePic = () => {
+    document.getElementById("pic").classList.add("hide");
+}
+const bounceBall = () => {
+    let ballElement = document.getElementById("ball");
+    ballElement.classList.add("pulse");
+};
+const productName = () => {
+    const productName = document.getElementById("txt-product-name").value;
+    const messageP = document.getElementById("product");
+    messageP.innerHTML = productName;
+
+    const comment = document.getElementById("txt-rating").value;
+    const messageB = document.getElementById("comment");
+    messageB.innerHTML = comment;
+
+    const rating = document.getElementById("txt-rating").value;
+    const messageA = document.getElementById("rating");
+    messageA.innerHTML = rating +"/5 Stars";
+
+    const userName = document.getElementById("txt-user-name").value;
+    const messageC = document.getElementById("user-name");
+    messageC.innerHTML = "by:" + userName;
+};
+
+
+/*
+const comment = () =>{
+    const comment = document.getElementById("txt-comment").value;
+    const messageA = document.getElementById("comment");
+    messageA.innerHTML = comment;
+}
+
+const rating = () =>{
+    const comment = document.getElementById("txt-rating").value;
+    const messageB = document.getElementById("rating");
+    messageB.innerHTML = comment;
+}
+
+const userName = () =>{
+    const userName = document.getElementById("txt-user-name").value;
+    const messageC = document.getElementById("user-name");
+    messageC.innerHTML = userName;
+}
+*/
+
+
+window.onload = () => {
+   document.getElementById("button-show").onclick = showPic;
+   document.getElementById("button-hide").onclick = hidePic;
+   document.getElementById("bounce-button").onclick = bounceBall;
+   document.getElementById("message-button").onclick = productName;
+}
---
+++ 242 CSCE/sept12/script.js
@@ -0,0 +1,19 @@
+const changeText = () => {
+    const helloP =document.getElementById("date");
+    helloP.innerHTML = "hi"
+    helloP.classList.add("special");
+}
+
+const showJasper = () => {
+    document.getElementById("jasper").classList.remove("hide");
+}
+
+const hideJasper = () => {
+    document.getElementById("jasper").classList.add("hide");
+}
+
+window.onload = () => {
+   document.getElementById("button-click").onclick = changeText;
+   document.getElementById("button-show").onclick = showJasper;
+   document.getElementById("button-hide").onclick = hideJasper;
+}
