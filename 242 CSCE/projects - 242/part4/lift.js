3a12a248adc838433c50149ac7f1da3818f7e541
Colin Mathis Elmore <colin_elmore@Colins-MacBook-Pro-3.local>
2023-10-29 18:00:43 -0400

    try

--- 242 CSCE/projects - 242/part4/lift.js
+++ 242 CSCE/projects - 242/part4/lift.js
@@ -20,37 +20,41 @@ const getLiftItem = (lift) => {
     liftSection.classList.add("lift");
     liftSection.classList.add(lift.category);
 
-    let img = document.createElement("img");
-    img.src = "projects - 242/part4/pictures/" + lift.img;
-  
     console.log(lift);
   
+    const img = document.createElement("img");
+    img.src = "https://colinelmore.github.io/242%20CSCE/projects%20-%20242/part4/lifts.json/" + lift.img;
+
     const h5 = document.createElement("h5");
     h5.innerText = lift.name;
+
     const p = document.createElement("p")
     p.innerText = lift.description;
+
     const button = document.createElement("button");
-    button.innerHTML = "Add Lift"
+    button.innerHTML = "Add Lift";
+
     liftSection.append(img);
     liftSection.append(h5);
     liftSection.append(p);
     liftSection.append(button);
     return liftSection;
-
-    
-
   };
 
 
 showLifts();
 
 const showLegs = () => {
-  document.getElementsByClassName("legs").classList.toggle("hidden");
-  console.log("works");
+  //document.getElementsByClassName("legs").classList.toggle("hidden");
+  
 };
 
+//document.getElementById("show-legs").onclick = showLegs;
+
+
 window.onload = () => {
-  document.getElementById("show-legs").onclick = showLegs;
-  //document.getElementById("lift-section").classList.add("hidden");
+ document.getElementById("legs").classList.add("hidden");
+  //document.getElementById("show-legs").onclick = showLegs;
+  console.log("works");
 }
 
